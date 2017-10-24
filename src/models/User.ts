d4d66effe984e78b3e42f5b43391641d8e23c1b1
Naim Jeem <naim36208@gmail.com>
2017-10-24 16:20:25 +0600

    Created User schema

---
+++ src/models/User.ts
@@ -0,0 +1,46 @@
+import { Schema, model } from 'mongoose';
+
+let UserSchema: Schema = new Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  firstName: {
+    type: String,
+    default: '',
+    required: true
+  },
+  lastName: {
+    type: String,
+    default: '',
+    required: true
+  },
+  username: {
+    type: String,
+    default: '',
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  email: {
+    type: String,
+    default: '',
+    required: true
+  },
+  password: {
+    type: String,
+    default: '',
+    required: true
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Post'
+  }]
+});
+
+
+export default model('User', UserSchema);
